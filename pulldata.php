e6cccbaf4da28668f08ac7a5c5f48b9e8ddbea72
JeffyDatoh <6110210071@psu.ac.th>
2021-08-18 19:06:09 +0700

    edit pulldata to json

--- pulldata.php
+++ pulldata.php
@@ -1,4 +1,8 @@
-<?php
+<?php    
+    
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type:application/json; charset=UTF-8");
+    
     $url = "https://nithi.cs.psu.ac.th/server-status?auto";
 
     $curl = curl_init();
@@ -9,10 +13,38 @@
     $data = curl_exec($curl);
 
     curl_close($curl);
-    print_r($data);
+    //print_r($data);
 
     $list = explode("\n",$data);
 
+    $Total_Accesses= explode(": ",$list[13]);
+    $Total_kBytes= explode(": ",$list[14]);
+    $CPU_System= explode(": ",$list[17]);
+    $CPU_Load= explode(": ",$list[20]);
+    $Uptime= explode(": ",$list[21]);
+    $Req_PerSec= explode(": ",$list[22]);
+    $Bytes_PerSec= explode(": ",$list[23]);
+    $Bytes_PerReq= explode(": ",$list[24]);
+    $Duration_PerReq= explode(": ",$list[25]);
+
+
+    $response = new stdClass();
+    $response->Total_Accesses=$Total_Accesses[1];
+    $response->Total_kBytes=$Total_kBytes[1];
+    $response->CPU_System=$CPU_System[1];
+    $response->CPU_Load=$CPU_Load[1];
+    $response->Uptime=$Uptime[1];
+    $response->Req_PerSec=$Req_PerSec[1];
+    $response->Bytes_PerSec=$Bytes_PerSec[1];
+    $response->Bytes_PerReq=$Bytes_PerReq[1];
+    $response->Duration_PerReq=$Duration_PerReq[1];
+    
+    $response_json = json_encode($response);
+    echo $response_json;
+
+
+
+    /*
     // show full data name and value
     echo "<br>","<br>";
     echo $list[13],"<br>";
@@ -24,28 +56,21 @@
     echo $list[23],"<br>";
     echo $list[24],"<br>";
     echo $list[25],"<br>";
+    */
 
-
+    /*
     // show only value
-    $Total_Accesses= explode(":",$list[13]);
-    $Total_kBytes= explode(":",$list[14]);
-    $CPUSystem= explode(":",$list[17]);
-    $CPULoad= explode(":",$list[20]);
-    $Uptime= explode(":",$list[21]);
-    $ReqPerSec= explode(":",$list[22]);
-    $BytesPerSec= explode(":",$list[23]);
-    $BytesPerReq= explode(":",$list[24]);
-    $DurationPerReq= explode(":",$list[25]);
-
     echo "<br>","<br>";
     echo $Total_Accesses[1],"<br>";
     echo $Total_kBytes[1],"<br>";
-    echo $CPUSystem[1],"<br>";
-    echo $CPULoad[1],"<br>";
+    echo $CPU_System[1],"<br>";
+    echo $CPU_Load[1],"<br>";
     echo $Uptime[1],"<br>";
-    echo $ReqPerSec[1],"<br>";
-    echo $BytesPerSec[1],"<br>";
-    echo $BytesPerReq[1],"<br>";
-    echo $DurationPerReq[1],"<br>";
-    
+    echo $Req_PerSec[1],"<br>";
+    echo $Bytes_PerSec[1],"<br>";
+    echo $Bytes_PerReq[1],"<br>";
+    echo $Duration_PerReq[1],"<br>";
+    */
+
+
 ?>
