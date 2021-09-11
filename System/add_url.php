b6b549edc512b01fb151abb9bd6ceb7d6e6975a2
JeffyDatoh <6110210071@psu.ac.th>
2021-09-11 13:36:39 +0700

    add url for get data

---
+++ System/add_url.php
@@ -0,0 +1,12 @@
+<form class="form1" method="post" action="data.php" id="form1" target="iframe_target">
+<iframe id="iframe_target" name="iframe_target" src="#" style="width:0;height:0;border:0px solid #fff;"></iframe>
+<fieldset>
+    <ul>
+            <p>Please enter your server to continue to the notify.</p>
+            <label for="name">server:</label><span><input type="text" name="url" placeholder="url server" class="required" role="input" aria-required="true"/></span>
+            <input  class="submit .transparentButton" value="Next" type="submit" name="Submit"/> 
+
+    </ul>
+    <br/>
+    </fieldset>
+</form>
