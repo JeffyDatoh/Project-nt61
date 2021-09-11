d81b645abfd4b50a3dc9aa043927e1ce7cd8ced7
Meolis <87792776+Meolis@users.noreply.github.com>
2021-09-11 20:33:59 +0700

    test with avg
    detect with avg and use line notify

---
+++ test-area/test_detect_with_AVG.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+<?php require 'notify.php';?>
+    <scrip>
+        if(avg_ta + 25 < total_ta  || avg_ta + 50 < total_ta) 
+            function_alert();
+        if(avg_tk + 25 < total_tk  || avg_tk + 50 < total_tk)  
+            function_alert();
+        if(avg_cpus + 25 < total_cpus || avg_cpus + 50 < total_cpus)   
+            function_alert();
+        if(avg_cpul + 25 < total_cpul || avg_cpul + 50 < total_cpul)  
+            function_alert();
+        if(avg_ut + 25 < total_ut || avg_ut + 50 < total_ut) 
+            function_alert();
+        if(avg_rps + 25 < total_rps || avg_rps + 50 < total_rps)  
+            function_alert();
+        if(avg_bps + 25 < total_bps || avg_bps + 50 < total_bps)  
+            function_alert();
+        if(avg_bpr + 25 < total_bpr || avg_bpr + 50 < total_bpr) 
+            function_alert();
+        if(avg_dpr + 25 < total_dpr || avg_dpr + 50 < total_dpr)  
+            function_alert();
+    </scrip>
+</body>
+</html>
